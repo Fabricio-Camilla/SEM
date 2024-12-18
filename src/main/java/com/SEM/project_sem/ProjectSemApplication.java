f5f20b64373cd358765f1ba5a554fa1e6611dbb0
Fabricio-Camilla <fachucamilla32@gmail.com>
2024-12-18 18:15:52 -0300

    initial commit

---
+++ src/main/java/com/SEM/project_sem/ProjectSemApplication.java
@@ -0,0 +1,13 @@
+package com.SEM.project_sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SEM/project_sem/ProjectSemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEM.project_sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
