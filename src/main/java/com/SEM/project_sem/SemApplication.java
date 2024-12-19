3cd3de4a61decbeac8f1d21b7d3766b7d4f3b253
Fabricio-Camilla <fachucamilla32@gmail.com>
2024-12-19 17:58:15 -0300

    build api

--- src/main/java/com/SEM/project_sem/SemApplication.java
+++ src/main/java/com/SEM/project_sem/SemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectSemApplication {
+public class SemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectSemApplication.class, args);
+		SpringApplication.run(SemApplication.class, args);
 	}
 
 }
